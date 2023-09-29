7bb3c9b9efc48153205c8b1c9c2d574827517675
Akhilesh Wagh <akhiwaghtech@gmail.com>
2023-09-29 18:53:20 +0530

    Added Next and previous image functionality to ImageViewer

--- Customised Image GalleryV2.php
+++ Customised Image GalleryV2.php
@@ -50,6 +50,39 @@
             color: #999;
         }
 
+
+        #imageViewer {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.9);
+            z-index: 9999;
+        }
+
+        .closeButton {
+            position: absolute;
+            top: 20px;
+            right: 20px;
+            font-size: 24px;
+            color: white;
+            cursor: pointer;
+        }
+
+        #displayedImage {
+            display: block;
+            margin: 0 auto;
+            max-width: 90%;
+            max-height: 90%;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+
+
         @media (max-width: 600px) {
             .gallery {
                 grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
@@ -77,26 +110,40 @@
         function LoadNextBatch(files, loadBatchSize) {
             var loadedImages = 0, currentIndex = 0;
             while (loadedImages < loadBatchSize && currentIndex < files.length) {
-                loadedImages += LoadImage(files[currentIndex])? 1 : 0;
+                loadedImages += LoadImage(files[currentIndex]) ? 1 : 0;
                 currentIndex++;
             }
+        }
+
+        function ProperizeNames(names_ary) {
+            let jsonArray = [];
+            for (let index = 0; index < names_ary.length; index++) {
+                name = names_ary[index].replace(/[^a-zA-Z0-9 ]/g, " ").toLowerCase();
+                jsonArray.push({ [names_ary[index]]: name });
+            }
+            return jsonArray;
         }
     </script>
 </head>
 
 <body>
     <header>
-        <input type="text" id="searchInput" placeholder="Search Images">
+        <input type="text" id="searchInput" onChange="filterImages(this.value)" placeholder="Search Images">
     </header>
 
     <div class="gallery" id="imageGallery"></div>
 
     <div id="imageViewer">
-
+        <span class="closeButton" onclick="document.getElementById('imageViewer').style.display='none'"
+            id="closeViewer">&times;</span>
+        <img onclick="ChangeImageViewer(event, true)" id="displayedImage" />
     </div>
     <script>
         // Variables Initlization
         const imageGallery = document.getElementById("imageGallery");
+        const displayedImage = document.getElementById("displayedImage");
+        const imageViewer = document.getElementById("imageViewer");
+
         const loadBatchSize = 10;
         var files = [<?php
         $directory = 'imgs/';
@@ -109,16 +156,65 @@
         }
         ?>];
         files = shuffleArray(files);
+        propernames = ProperizeNames(files);
 
         // Initial Load
         for (let index = 0; index < loadBatchSize; index++) {
             LoadImage(files[index]);
         }
 
-
         // Functions
+        function getImagesOnPage() {
+            return Array.from(document.getElementById('imageGallery').querySelectorAll('img'));
+        }
+
+        // TODO: filterImages
+        function filterImages(query) {
+
+        }
+
+        function ChangeImageViewer(event, isimage = false) {
+            x = event.pageX;
+            if (event.target.tagName.toLowerCase() === 'span') {
+                return;
+            }
+            let width = event.target.offsetWidth;
+            let left = event.target.offsetLeft;
+            let NEXTimage = 0; //1=next -1=prev
+            if (isimage) {
+                if (x < (width / 2)) { NEXTimage = -1; }
+                else { NEXTimage = 1; }
+            }
+            else if (x - left < width / 2) {
+                NEXTimage = -1;
+            } else {
+                NEXTimage = 1;
+            }
+
+            let imageArray = getImagesOnPage();
+            indexOFcurImage = 0;
+            imageArray.forEach(function (element, index) {
+                if(displayedImage.src == element.src){
+                    indexOFcurImage = index;
+                }
+            });
+            indexOFcurImage += NEXTimage;
+            if (indexOFcurImage < 0 || indexOFcurImage >= imageArray.length) {
+                return;
+            }
+            else{
+                UpdateImageViewer(imageArray[indexOFcurImage].src);
+            }
+        }
+        function UpdateImageViewer(url, show = false) {
+            displayedImage.src = url;
+            if (show) {
+                imageViewer.style.display = "block";
+            }
+        }
+
         function LoadImage(url) {
-            let imageArray = Array.from(document.getElementById('imageGallery').querySelectorAll('img'));
+            let imageArray = getImagesOnPage();
             let loadimage = true;
 
             // Itrating through the image array to check if the image already exists
@@ -129,19 +225,21 @@
                     break;
                 }
             }
-            
+
             if (loadimage) {
                 const img = new Image();
                 img.src = url;
                 img.alt = url;
                 img.style.gridColumn = "span 2";
                 img.style.gridRow = "span 2";
+                img.addEventListener("click", () => {
+                    UpdateImageViewer(url, true);
+                });
                 imageGallery.appendChild(img);
                 return true;
             }
             return false;
         }
-
 
         // Event Listners
         window.onscroll = function (ev) {
@@ -150,6 +248,11 @@
             }
         };
 
+        document.addEventListener("click", function (event) {
+            if (imageViewer.style.display != "none" && event.target.id != "displayedImage" && event.target.id == "imageViewer") {
+                ChangeImageViewer(event);
+            }
+        });
     </script>
 </body>
 
